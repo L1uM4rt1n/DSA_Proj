5f0474f9e67bbcdb1606e103f8ec35dc14f8eb92
Pramit008 <pramits.2022@scis.smu.edu.sg>
2023-11-01 17:19:21 +0800

    updated quadtree compression
    ...

--- src/App.java
+++ src/App.java
@@ -1,6 +1,4 @@
-import java.io.*;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
--- src/Utility.java
+++ src/Utility.java
@@ -1,74 +1,84 @@
-
-
 import java.io.*;
-import java.util.*;
+import java.awt.Color;
 
 public class Utility implements Serializable {
 
-    public void Compress(int[][][] pixels, String outputFileName) throws IOException {
-        Quadtree root = buildQuadtree(pixels, 0, 0, pixels.length, pixels[0].length);
-        root.size = pixels.length;  // Ensure the root size is set
+    static int threshold = 4; // Default threshold value
+    static int MIN_SZ = 6; // Minimum size for quadtree division
 
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFileName))) { 
-            oos.writeObject(root); 
+    public void Compress(int[][][] pixels, String outputFileName, int threshold) throws IOException {
+        Node root = buildQuadtree(pixels, 0, 0, pixels.length, pixels[0].length, threshold);
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFileName))) {
+            oos.writeObject(root);
         }
     }
 
-    private Quadtree buildQuadtree(int[][][] pixels, int x, int y, int width, int height) {
-        if (isUniform(pixels, x, y, width, height) || width == 1 || height == 1) {
-            QuadtreeLeaf leaf = new QuadtreeLeaf(getAverageColor(pixels, x, y, width, height));
-            leaf.size = Math.max(width, height);  // Set the size for the leaf
-            return leaf;
+    // Overloaded Compress method without threshold
+    public void Compress(int[][][] pixels, String outputFileName) throws IOException {
+        Compress(pixels, outputFileName, threshold);
+    }
+
+    private Node buildQuadtree(int[][][] pixels, int x, int y, int width, int height, int threshold) {
+        if (entropy(pixels, x, y, width, height) < threshold || width <= MIN_SZ || height <= MIN_SZ) {
+            int[] color = getAverageColor(pixels, x, y, width, height);
+            return new Node(color, null, x, y, width, height);
         }
 
         int halfWidth = width / 2;
         int halfHeight = height / 2;
 
-        Quadtree topLeft = buildQuadtree(pixels, x, y, halfWidth, halfHeight);
-        Quadtree topRight = buildQuadtree(pixels, x + halfWidth, y, halfWidth, halfHeight);
-        Quadtree bottomLeft = buildQuadtree(pixels, x, y + halfHeight, halfWidth, halfHeight);
-        Quadtree bottomRight = buildQuadtree(pixels, x + halfWidth, y + halfHeight, halfWidth, halfHeight);
-
-        QuadtreeNode node = new QuadtreeNode(topLeft, topRight, bottomLeft, bottomRight);
-        node.size = Math.max(width, height);  // Set the size for the internal node
-        return node;
+        return new Node(null, new Node[]{
+                buildQuadtree(pixels, x, y, halfWidth, halfHeight, threshold),
+                buildQuadtree(pixels, x + halfWidth, y, width - halfWidth, halfHeight, threshold),
+                buildQuadtree(pixels, x, y + halfHeight, halfWidth, height - halfHeight, threshold),
+                buildQuadtree(pixels, x + halfWidth, y + halfHeight, width - halfWidth, height - halfHeight, threshold)
+        }, x, y, width, height);
     }
-    
-    private boolean isUniform(int[][][] pixels, int x, int y, int width, int height) {
-        int[] firstColor = pixels[x][y];
+
+    private double entropy(int[][][] pixels, int x, int y, int width, int height) {
+        double s = 0;
+        int[] avg = getAverageColor(pixels, x, y, width, height);
+        Color avgColor = new Color(avg[0], avg[1], avg[2]);
+
         for (int i = x; i < x + width; i++) {
             for (int j = y; j < y + height; j++) {
-                if (!Arrays.equals(pixels[i][j], firstColor)) {
-                    return false;
-                }
+                Color c = new Color(pixels[i][j][0], pixels[i][j][1], pixels[i][j][2]);
+                s += Math.abs(avgColor.getRed() - c.getRed());
+                s += Math.abs(avgColor.getGreen() - c.getGreen());
+                s += Math.abs(avgColor.getBlue() - c.getBlue());
             }
         }
-        return true;
+        return (double) s / (width * height);
     }
 
     private int[] getAverageColor(int[][][] pixels, int x, int y, int width, int height) {
-        long totalRed = 0, totalGreen = 0, totalBlue = 0;
-        int pixelCount = width * height;
+        int sumRed = 0;
+        int sumGreen = 0;
+        int sumBlue = 0;
+        int count = 0;
 
         for (int i = x; i < x + width; i++) {
             for (int j = y; j < y + height; j++) {
-                totalRed += pixels[i][j][0];
-                totalGreen += pixels[i][j][1];
-                totalBlue += pixels[i][j][2];
+                sumRed += pixels[i][j][0];
+                sumGreen += pixels[i][j][1];
+                sumBlue += pixels[i][j][2];
+                count++;
             }
         }
-
-        return new int[] {(int) (totalRed / pixelCount), (int) (totalGreen / pixelCount), (int) (totalBlue / pixelCount)};
+        return new int[]{sumRed / count, sumGreen / count, sumBlue / count};
     }
+
+   
 
     public int[][][] Decompress(String inputFileName) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFileName))) {
             Object object = ois.readObject();
 
-            if (object instanceof Quadtree) {
-                Quadtree root = (Quadtree) object;
-                int[][][] decompressedPixels = new int[root.size][root.size][3];
-                decompressQuadtree(root, decompressedPixels, 0, 0, root.size, root.size);
+            if (object instanceof Node) {
+                Node root = (Node) object;
+                int[][][] decompressedPixels = new int[root.width][root.height][3];
+                decompressQuadtree(root, decompressedPixels, 0, 0, root.width, root.height);
                 return decompressedPixels;
             } else {
                 throw new IOException("Invalid object type in the input file");
@@ -76,46 +86,40 @@ public class Utility implements Serializable {
         }
     }
 
-    private void decompressQuadtree(Quadtree node, int[][][] pixels, int x, int y, int width, int height) {
-        if (node instanceof QuadtreeLeaf) {
-            int[] color = ((QuadtreeLeaf) node).color;
+    private void decompressQuadtree(Node node, int[][][] pixels, int x, int y, int width, int height) {
+        if (node.children == null) {
+            int[] color = node.color;
             for (int i = x; i < x + width; i++) {
                 for (int j = y; j < y + height; j++) {
                     pixels[i][j] = color;
                 }
             }
-        } else if (node instanceof QuadtreeNode) {
-            QuadtreeNode internal = (QuadtreeNode) node;
+        } else {
             int halfWidth = width / 2;
             int halfHeight = height / 2;
 
-            decompressQuadtree(internal.topLeft, pixels, x, y, halfWidth, halfHeight);
-            decompressQuadtree(internal.topRight, pixels, x + halfWidth, y, halfWidth, halfHeight);
-            decompressQuadtree(internal.bottomLeft, pixels, x, y + halfHeight, halfWidth, halfHeight);
-            decompressQuadtree(internal.bottomRight, pixels, x + halfWidth, y + halfHeight, halfWidth, halfHeight);
+            decompressQuadtree(node.children[0], pixels, x, y, halfWidth, halfHeight);
+            decompressQuadtree(node.children[1], pixels, x + halfWidth, y, width - halfWidth, halfHeight);
+            decompressQuadtree(node.children[2], pixels, x, y + halfHeight, halfWidth, height - halfHeight);
+            decompressQuadtree(node.children[3], pixels, x + halfWidth, y + halfHeight, width - halfWidth,
+                    height - halfHeight);
         }
     }
 
-    private abstract class Quadtree implements Serializable {
-        int size;
-    }
-
-    private class QuadtreeLeaf extends Quadtree {
+    // Node class
+    private static class Node implements Serializable {
         int[] color;
+        Node[] children;
+        int x, y, width, height;
 
-        QuadtreeLeaf(int[] color) {
+        public Node(int[] color, Node[] children, int x, int y, int width, int height) {
             this.color = color;
-        }
-    }
-
-    private class QuadtreeNode extends Quadtree {
-        Quadtree topLeft, topRight, bottomLeft, bottomRight;
-
-        QuadtreeNode(Quadtree topLeft, Quadtree topRight, Quadtree bottomLeft, Quadtree bottomRight) {
-            this.topLeft = topLeft;
-            this.topRight = topRight;
-            this.bottomLeft = bottomLeft;
-            this.bottomRight = bottomRight;
+            this.children = children;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
         }
     }
 }
+
